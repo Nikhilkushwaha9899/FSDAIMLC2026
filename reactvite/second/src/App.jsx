70cce74121ccb516b7540414be3ec9dfbdc86477
Nikhilkushwaha9899 <kushwahanikhil616@gmail.com>
2026-09-24 10:35:45 +0530

    React vite

--- reactvite/second/src/App.jsx
+++ reactvite/second/src/App.jsx
@@ -3,14 +3,13 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import Icard from './component/Icard'
-import IcardGallery from './component/IcardGallery'
+import ImdbCard from './component/ImdbCard'
 
 function App() {
-  
+
   return (
-    <div style={{ border: '7px solid white',height:'auto'}}>
-    <IcardGallery></IcardGallery>
+    <div>
+      <ImdbCard></ImdbCard>
     </div>
   )
 }
---
+++ reactvite/second/src/component/ImdbCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function ImdbCard(){
+    return (
+      <div>
+        <div style={{}}>
+            <h1>
+                Imdb 2025
+            </h1>
+        </div>
+      </div>
+    )
+}
+export default ImdbCard
